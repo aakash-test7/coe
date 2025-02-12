a4f2d41e2a2d452a4f4bf5ff97dc332bcc7e99b5
Aakash Kharb <akgdsc7@gmail.com>
2025-02-12 13:11:03 +0530

    Create __init__.py

---
+++ pages/__init__.py
@@ -0,0 +1,7 @@
+from pages.About import about_page
+from pages.Tutorials import tutorials_page
+from pages.Glossary import glossary_page
+from pages.Home_Page import home_page
+from pages.Meta_Data import meta_data_page
+from pages.Search import search_page
+from pages.Citations import citations_page
