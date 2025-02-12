a785c8e60a2ade3f6e439dbd32b3627599b3b248
Aakash Kharb <akgdsc7@gmail.com>
2025-02-12 13:27:19 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import base64
