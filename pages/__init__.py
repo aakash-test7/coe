08713e8d104150fef739628c69a736a8a44656ed
Aakash Kharb <akgdsc7@gmail.com>
2025-02-13 17:08:48 +0530

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -5,3 +5,4 @@ from pages.Home_Page import home_page
 from pages.Meta_Data import meta_data_page
 from pages.Search import search_page
 from pages.Citations import citations_page
+from pages.Login import login_page
