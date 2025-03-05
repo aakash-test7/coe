9ebc29b1e0a0c75f55777bc09939546b59f1793b
Aakash Kharb <akgdsc7@gmail.com>
2025-03-05 21:55:41 +0530

    Update About_Us.py

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -1,19 +1,9 @@
 import streamlit as st
 from pages.footer_all import base_footer 
+from pages.about_us_html import about_us_html
 
 def about_page():
-    st.title("About Us")
-    st.write("**Learn more about the application and its developers.**")
-
-    import urllib.parse
-    with st.popover('Contact Us'):
-        email_to = "gopalkalwan56@gmaill.com"
-        subject = "MultiClassClassificationInput App Inquiry"
-        body = "I am writing to inquire about..."
-        subject_encoded = urllib.parse.quote(subject)
-        body_encoded = urllib.parse.quote(body)
-        mailto_link = f"mailto:{email_to}?subject={subject_encoded}&body={body_encoded}"
-        st.markdown(f"[Tap the link to open E-mail](mailto:{email_to}?subject={subject_encoded}&body={body_encoded})")
+    about_us_html()
     base_footer()
 
 if __name__ == "__main__":
