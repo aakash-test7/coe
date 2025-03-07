f829afaf346cf9498be931dc7346a8a0fe83adce
Aakash Kharb <akgdsc7@gmail.com>
2025-03-07 21:23:58 +0530

    Update Home_Page.py

--- pages/Home_Page.py
+++ pages/Home_Page.py
@@ -7,7 +7,7 @@ def home_page():
     home_html()
 
     col1,col2=st.columns(2)
-    st.markdown("""<style>.stVerticalBlock.st-key-con11hp {background-color: #DAF7A6; padding: 20px; border-radius: 10px;} </style>""", unsafe_allow_html=True)
+    st.markdown("""<style>.stVerticalBlock.st-key-con11hp {background-color: rgba(1,171,228,1); padding: 20px; border-radius: 10px; transition: all 0.3s ease-in-out;} .stVerticalBlock.st-key-con11hp:hover {background-color: rgba(1,171,228,0.5); box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2); transform: translateY(-2px);} </style>""", unsafe_allow_html=True)
     con=col1.container(border=True,key="con11hp")
     with con:
         st.write(".")
@@ -17,7 +17,7 @@ def home_page():
         st.write(".")
         st.write(".")
     con=col2.container(border=True,key="con12hp")
-    st.markdown("""<style>.stVerticalBlock.st-key-con12hp {background-color: #F1F7A6; padding: 20px; border-radius: 10px;} </style>""", unsafe_allow_html=True)
+    st.markdown("""<style>.stVerticalBlock.st-key-con12hp {background-color: rgba(255,255,205,1); padding: 20px; border-radius: 10px; transition: all 0.3s ease-in-out;} .stVerticalBlock.st-key-con12hp:hover {background-color: rgba(255,255,205,0.5); box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2); transform: translateY(-2px);} </style>""", unsafe_allow_html=True)
     with con:
         st.write(".")
         st.write(".")
@@ -29,7 +29,7 @@ def home_page():
     con=st.container(border=True)
     with con:
         gallery_html()
-
+    st.markdown("""<style>.stVerticalBlock.st-key-con2hp {background-color: rgba(242,240,239,1); padding: 20px; border-radius: 10px; transition: all 0.3s ease-in-out;} .stVerticalBlock.st-key-con2hp:hover {background-color: rgba(242,240,239,0.5); box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2); transform: translateY(-2px);} </style>""", unsafe_allow_html=True)
     con=st.container(border=True,key="con2hp")
     with con:
         st.write(".")
@@ -40,7 +40,7 @@ def home_page():
         st.write(".")
 
     col1,col2=st.columns(2)
-    st.markdown("""<style>.stVerticalBlock.st-key-con31hp {background-color: #DAF7A6; padding: 20px; border-radius: 10px;} </style>""", unsafe_allow_html=True)
+    st.markdown("""<style>.stVerticalBlock.st-key-con31hp {background-color: rgba(195,216,161,1); padding: 20px; border-radius: 10px; transition: all 0.3s ease-in-out;} .stVerticalBlock.st-key-con31hp:hover {background-color: rgba(195,216,161,0.5); box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2); transform: translateY(-2px);} </style>""", unsafe_allow_html=True)
     con=col1.container(border=True,key="con31hp")
     with con:
         st.write(".")
@@ -49,7 +49,7 @@ def home_page():
         st.write(".")
         st.write(".")
         st.write(".")
-    st.markdown("""<style>.stVerticalBlock.st-key-con32hp {background-color: #F1F7A6; padding: 20px; border-radius: 10px;} </style>""", unsafe_allow_html=True)
+    st.markdown("""<style>.stVerticalBlock.st-key-con32hp {background-color: rgba(255,119,75,1); padding: 20px; border-radius: 10px; transition: all 0.3s ease-in-out;} .stVerticalBlock.st-key-con32hp:hover {background-color: rgba(255,119,75,0.5); box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2); transform: translateY(-2px);} </style>""", unsafe_allow_html=True)
     con=col2.container(border=True,key="con32hp")
     with con:
         st.write(".")
