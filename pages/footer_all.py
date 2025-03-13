04dc68ab47fdfd6f030ee2d7b58b6c0405c21355
Aakash Kharb <akgdsc7@gmail.com>
2025-03-13 13:39:59 +0530

    Update footer_all.py

--- pages/footer_all.py
+++ pages/footer_all.py
@@ -1,12 +1,10 @@
 import streamlit as st
-import base64
 from backend import generate_signed_url, img_to_base64
 import requests
 
-
 def base_footer():
-    file_url=generate_signed_url('Logos/mdu.png')
-    response=requests.get(file_url)
+    file_url = generate_signed_url('Logos/mdu.png')
+    response = requests.get(file_url)
     img_base64 = img_to_base64(response.content)
     footer_image = f"data:image/png;base64,{img_base64}"
     footer = f"""
@@ -45,7 +43,7 @@ def base_footer():
             left: 0;
             padding: 0 0;  /* No horizontal padding */
             z-index: 9999; /* Ensures the footer is above other content */
-            max-height:280px;
+            max-height: 280px;
         }}
     
         .container {{
@@ -84,18 +82,24 @@ def base_footer():
             flex-basis: 15%;
             padding: 5px;
             margin-bottom: 2px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            text-align: center; /* Ensure text is centered */
         }}
     
         .col-3 h3 {{
             color: #fff;
             font-size: 25px;
-            align-items: center;
             margin-top: 10px;
+            margin-bottom: 10px; /* Add spacing between h3 and image */
         }}
     
         .col-3 img {{
-            width: 125px;
-            magin-bottom-10px;
+            width: 125px; /* Fixed width */
+            height: auto; /* Maintain aspect ratio */
+            min-width: 125px; /* Prevent shrinking */
         }}
     
         .container a {{
@@ -115,38 +119,14 @@ def base_footer():
             padding-bottom: 2px;
             text-align: center;
         }}
-        @media only screen and (max-width: 600px) {{
-            .footer-container {{
-                max-height: 260px;
-            }}
-    
-            .col-1 img {{
-                width: 40px;
-            }}
-    
-            .col-1 p {{
-                font-size: 14px;
-            }}
-    
-            .col-3 h3 {{
-                font-size: 18px;
-            }}
-    
-            .col-3 img {{
-                width: 125px;
-            }}
-    
-            .footer-2 p {{
-                font-size: 13px;
-            }}
-        }}
+        
     </style>
     
     <div class="footer-container">
         <div class="container">
             <div class="col-1">
                 <p><br><br><br>Stress Physiology & Molecular Biology Lab,<br>Centre for Biotechnology,<br>Maharshi Dayanand University, Rohtak, HR, INDIA<br>E-mail : <a href="mailto:ssgill14@mdurohtak.ac.in" style="text-decoration: none;">ssgill14@mdurohtak.ac.in</a><br>
-            <a href="mailto:kduiet@mdurohtak.ac.in " style="text-decoration: none;">kduiet@mdurohtak.ac.in</a></p>
+            <a href="mailto:kduiet@mdurohtak.ac.in " style="text-decoration: none;">⠀⠀⠀⠀⠀kduiet@mdurohtak.ac.in</a></p>
             </div>
             <div class="col-3">
                 <h3><a href="https://mdu.ac.in/default.aspx" style="text-decoration: none;" target="_blank">MDU</a></h3>
